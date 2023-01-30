807ddc7406187d74261a1194310f529146ea20c2
Ganeshkumar <123441945+GaneshTRL748@users.noreply.github.com>
2023-01-30 18:24:46 +0530

    Update Bankoperation.java

--- Exceptionhandling/src/main/java/bankoperation/Bankoperation.java
+++ Exceptionhandling/src/main/java/bankoperation/Bankoperation.java
@@ -73,11 +73,11 @@ public class Bankoperation{
         Bankoperation a1=new Bankoperation("ganesh",1247170000153950L,200,1685);
         a1.deposit(500);
         a1.withdraw(500);
-        l.info("Hi "+a1.accname+"Your Current balance:"+a1.balanceamount());
+        l.log("Hi "+a1.accname+"Your Current balance:"+a1.balanceamount());
         Bankoperation a2=new Bankoperation("Ruby",124717000153951L,5000,1247);
         a2.deposit(2000);
         a2.withdraw(10000);
-        l.info("Hi "+a2.accname+" Your Current balance:"+a2.balanceamount());
+        l.log("Hi "+a2.accname+" Your Current balance:"+a2.balanceamount());
     }
 }
 
