60ce0f2b24f23286c6f0620ae2dc175417d663ad
Ganeshkumar <123441945+GaneshTRL748@users.noreply.github.com>
2023-01-30 18:22:52 +0530

    Delete Stringformatexecption.java

--- Exceptionhandling/src/main/java/bankoperation/Stringformatexecption.java
+++
@@ -1,8 +0,0 @@
-package bankoperation;
-public class Stringformatexecption extends Exception {
-	public Stringformatexecption(String message)
-	{
-		super(message);
-	}
-    //System.out.print("exception");
-}
